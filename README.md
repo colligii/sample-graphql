669f2324c4088d6995de4ac62a04e8a0e31900ee
colligii <cgiovanne45@gmail.com>
2021-07-21 22:00:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# A GraphQL API.
+
+## About this project.
+
+_"I'm learning a little bit more of RESTful API's, searching about this, i found a facebook programming query language."_
+
+## Why
+
+This project is for my personal portfolio, and show my skils in GraphQL.
+
+[Link to see my contacts](https://colligii.github.io/#contacts)
+
+## Getting Started.
+
+### Prerequisites
+
+* Node.js installed on machine.
+
+## Installing
+
+```
+$ git clone https://github.com/colligii/sample-graphql.git
+$ cd sample-graphql
+$ yarn start
+```
+
+# Happy Hacking 😎😎😎😎
---
+++ src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const fs = require('fs');
+
+var schema;
+
+const data = fs.readFileSync(`${__dirname}/querys/graphql.gql`, { encoding: 'utf8' })
+
+schema = buildSchema(data);
+
+const port = process.env.PORT || 4000;
+
+const username = [
+    {
+        user: '_colli.gi',
+        pass: '123'
+    }
+]
+
+
+var root = {
+    getUser: ({user}) => {
+        const userGraphQL = username.map(item => {
+            if(item.user === user) return item
+        })
+        if(userGraphQL.length !== 1) {
+            throw new Error('User not found')
+        } else {
+            return userGraphQL[0]
+        }
+    }
+
+}
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(port);
+console.log(`GraphQL SERVER IS RUNNING AT PORT ${port}`);
